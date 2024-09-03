90dece683054cced4b0c84e11d81beedc74fd8fa
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-03 20:25:15 +0330

    Setup GRDB

---
+++ There/Data/Database.swift
@@ -0,0 +1,139 @@
+import Foundation
+import GRDB
+import os.log
+
+///
+/// You create an `AppDatabase` with a connection to an SQLite database
+/// (see <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/databaseconnections>).
+///
+/// Create those connections with a configuration returned from
+/// `AppDatabase/makeConfiguration(_:)`.
+///
+/// For example:
+///
+/// ```swift
+/// // Create an in-memory AppDatabase
+/// let config = AppDatabase.makeConfiguration()
+/// let dbQueue = try DatabaseQueue(configuration: config)
+/// let appDatabase = try AppDatabase(dbQueue)
+/// ```
+struct AppDatabase {
+    /// Creates an `AppDatabase`, and makes sure the database schema
+    /// is ready.
+    ///
+    /// - important: Create the `DatabaseWriter` with a configuration
+    ///   returned by ``makeConfiguration(_:)``.
+    init(_ dbWriter: any DatabaseWriter) throws {
+        self.dbWriter = dbWriter
+        try migrator.migrate(dbWriter)
+    }
+
+    /// Provides access to the database.
+    ///
+    /// Application can use a `DatabasePool`, while SwiftUI previews and tests
+    /// can use a fast in-memory `DatabaseQueue`.
+    ///
+    /// See <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/databaseconnections>
+    let dbWriter: any DatabaseWriter
+}
+
+// MARK: - Database Configuration
+
+extension AppDatabase {
+    private static let sqlLogger = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "SQL")
+
+    /// Returns a database configuration suited for `PlayerRepository`.
+    ///
+    /// SQL statements are logged if the `SQL_TRACE` environment variable
+    /// is set.
+    ///
+    /// - parameter base: A base configuration.
+    public static func makeConfiguration(_ base: Configuration = Configuration()) -> Configuration {
+        var config = base
+
+        // An opportunity to add required custom SQL functions or
+        // collations, if needed:
+        // config.prepareDatabase { db in
+        //     db.add(function: ...)
+        // }
+
+        // Log SQL statements if the `SQL_TRACE` environment variable is set.
+        // See <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/database/trace(options:_:)>
+        if ProcessInfo.processInfo.environment["SQL_TRACE"] != nil {
+            config.prepareDatabase { db in
+                db.trace {
+                    // It's ok to log statements publicly. Sensitive
+                    // information (statement arguments) are not logged
+                    // unless config.publicStatementArguments is set
+                    // (see below).
+                    os_log("%{public}@", log: sqlLogger, type: .debug, String(describing: $0))
+                }
+            }
+        }
+
+        #if DEBUG
+            // Protect sensitive information by enabling verbose debugging in
+            // DEBUG builds only.
+            // See <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/configuration/publicstatementarguments>
+            config.publicStatementArguments = true
+        #endif
+
+        return config
+    }
+}
+
+// MARK: - Database Migrations
+
+extension AppDatabase {
+    /// The DatabaseMigrator that defines the database schema.
+    ///
+    /// See <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/migrations>
+    private var migrator: DatabaseMigrator {
+        var migrator = DatabaseMigrator()
+
+        #if DEBUG
+            // Speed up development by nuking the database when migrations change
+            // See <https://swiftpackageindex.com/groue/grdb.swift/documentation/grdb/migrations>
+            migrator.eraseDatabaseOnSchemaChange = true
+        #endif
+
+        // Migrations for future application versions will be inserted here:
+        // migrator.registerMigration(...) { db in
+        //     ...
+        // }
+
+        return migrator
+    }
+}
+
+// MARK: - Database Access: Writes
+
+// The write methods execute invariant-preserving database transactions.
+
+extension AppDatabase {
+    /// A validation error that prevents some players from being saved into
+    /// the database.
+    enum ValidationError: LocalizedError {
+        case missingName
+
+        var errorDescription: String? {
+            switch self {
+            case .missingName:
+                return "Please provide a name"
+            }
+        }
+    }
+}
+
+// MARK: - Database Access: Reads
+
+// This demo app does not provide any specific reading method, and instead
+// gives an unrestricted read-only access to the rest of the application.
+// In your app, you are free to choose another path, and define focused
+// reading methods.
+extension AppDatabase {
+    /// Provides a read-only access to the database
+    var reader: DatabaseReader {
+        dbWriter
+    }
+}
