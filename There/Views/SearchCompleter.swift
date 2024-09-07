21b9d16fa6f0a8cc7713b192014503b77c498eaf
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-07 18:03:13 +0330

    Add search completer to find city and country

--- There/Views/SearchCompleter.swift
+++ There/Views/SearchCompleter.swift
@@ -2,10 +2,23 @@ import CoreLocation
 import MapKit
 import SwiftUI
 
+/// Represents a search result with additional information
+struct SearchResult: Identifiable {
+    let id = UUID()
+    let title: String
+    let subtitle: String
+    let completion: MKLocalSearchCompletion
+}
+
+/// A class that handles location search completions
 class SearchCompleter: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
-    @Published var results: [MKLocalSearchCompletion] = []
+    /// Published array of search results
+    @Published var results: [SearchResult] = []
+
+    /// The MKLocalSearchCompleter instance used for generating search suggestions
     private let completer: MKLocalSearchCompleter
 
+    /// Initializes the SearchCompleter
     override init() {
         completer = MKLocalSearchCompleter()
         super.init()
@@ -13,14 +26,29 @@ class SearchCompleter: NSObject, ObservableObject, MKLocalSearchCompleterDelegat
         completer.resultTypes = .address
     }
 
+    /// Initiates a search with the given query
+    /// - Parameter query: The search query string
     func search(_ query: String) {
         completer.queryFragment = query
     }
 
+    /// Delegate method called when the completer updates its results
+    /// - Parameter completer: The MKLocalSearchCompleter that updated its results
     func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        results = completer.results
+        // Map the MKLocalSearchCompletion results to our custom SearchResult struct
+        results = completer.results.map { completion in
+            SearchResult(
+                title: completion.title,
+                subtitle: completion.subtitle,
+                completion: completion
+            )
+        }
     }
 
+    /// Delegate method called when the completer encounters an error
+    /// - Parameters:
+    ///   - completer: The MKLocalSearchCompleter that encountered an error
+    ///   - error: The error that occurred
     func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
         print("Error: \(error.localizedDescription)")
     }
