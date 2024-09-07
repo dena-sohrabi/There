b75c0ed06a459c9e001915efbaa0aa4fe8ab88f8
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-07 18:57:03 +0330

    Update add button action

--- There/Views/AddButton.swift
+++ There/Views/AddButton.swift
@@ -5,30 +5,9 @@ struct AddButton: View {
     @Environment(\.openWindow) var openWindow
 
     var body: some View {
-        Menu {
-            Button("Add Person") {
-                openWindow(id: "add-person")
-            }
-            Button("Add Place") {
-                openWindow(id: "add-place")
-            }
-        } label: {
-            Text("Add")
-                .foregroundColor(addHovered ? .primary : .primary.opacity(0.8))
+        CompactButton(title: "Add") {
+            openWindow(id: "add-timezone")
         }
-        .menuStyle(.borderlessButton)
-        .menuIndicator(.hidden)
-        .fixedSize()
-        .padding(.horizontal, 6)
-        .frame(height: 28)
-        .background(addHovered ? .white : .white.opacity(0.8))
-        .onHover { hovering in
-            withAnimation {
-                addHovered = hovering
-            }
-        }
-        .cornerRadius(8)
-        .shadow(color: .primary.opacity(0.06), radius: 1, x: 0, y: addHovered ? 2 : 1)
     }
 }
 
