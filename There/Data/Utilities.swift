2f38d086424f1a989356dd74817208949bff58f6
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-13 17:33:40 +0330

    Add photo selection and utils class

--- There/Data/Utilities.swift
+++ There/Data/Utilities.swift
@@ -1,15 +1,31 @@
 import Foundation
+import SwiftUI
 
-/// Converts a two-letter country code to its corresponding emoji flag.
-///
-/// - Parameter countryCode: ISO 3166-1 alpha-2 country code.
-/// - Returns: Emoji representation of the country's flag.
-func getCountryEmoji(for countryCode: String) -> String {
-    // Unicode offset for Regional Indicator Symbols
-    let base: UInt32 = 127397
+class Utils {
+    public static var shared = Utils()
+    func selectPhoto() -> NSImage? {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedContentTypes = [.image]
+        openPanel.allowsMultipleSelection = false
 
-    // Convert each letter to its corresponding Regional Indicator Symbol
-    return countryCode.uppercased().unicodeScalars.map {
-        String(UnicodeScalar(base + $0.value)!)
-    }.joined()
+        if openPanel.runModal() == .OK, let url = openPanel.url {
+            return NSImage(contentsOf: url)
+        } else {
+            return nil
+        }
+    }
+
+    /// Converts a two-letter country code to its corresponding emoji flag.
+    ///
+    /// - Parameter countryCode: ISO 3166-1 alpha-2 country code.
+    /// - Returns: Emoji representation of the country's flag.
+    func getCountryEmoji(for countryCode: String) -> String {
+        // Unicode offset for Regional Indicator Symbols
+        let base: UInt32 = 127397
+
+        // Convert each letter to its corresponding Regional Indicator Symbol
+        return countryCode.uppercased().unicodeScalars.map {
+            String(UnicodeScalar(base + $0.value)!)
+        }.joined()
+    }
 }
