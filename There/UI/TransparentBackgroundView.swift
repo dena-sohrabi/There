4501ad4d79741af85e16f84bcafd102bc3e6ee2d
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-13 16:47:17 +0330

    Update TransparentBackgroundView blending mode

--- There/UI/TransparentBackgroundView.swift
+++ There/UI/TransparentBackgroundView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct TransparentBackgroundView: NSViewRepresentable {
     func makeNSView(context: Context) -> NSVisualEffectView {
         let view = NSVisualEffectView()
-        view.blendingMode = .behindWindow
+        view.blendingMode = .withinWindow
         view.state = .active
         view.material = .popover
         return view
