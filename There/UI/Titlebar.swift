dd0295c29d3a29df82aff272256005d4c8c73556
Dena Sohrabi <denasohrabi62@gmail.com>
2024-09-16 16:03:55 +0330

    Refactor Titlebar UI layout

--- There/UI/Titlebar.swift
+++ There/UI/Titlebar.swift
@@ -4,38 +4,39 @@ struct Titlebar: View {
     @EnvironmentObject var router: Router
     @State var hovered: Bool = false
     var body: some View {
-        Button {
-            router.cleanActiveRoute()
-        } label: {
-            HStack(alignment: .center) {
+        HStack(alignment: .center) {
+            Button {
+                router.cleanActiveRoute()
+            } label: {
                 Image(systemName: "chevron.left")
                     .foregroundColor(.secondary)
                     .font(.body)
-                switch router.activeRoute {
-                case .addTimezone:
-                    Text("🗺️")
-                        .font(.callout)
-                    Text("Add Time Zone")
-                        .font(.body)
-                        .fontWeight(.medium)
-                case .editTimeZone:
-                    Text("✍️")
-                        .font(.callout)
-                    Text("Edit Time Zone")
-                        .font(.body)
-                        .fontWeight(.medium)
-                case .mainView:
-                    EmptyView()
-                }
+                    .padding(4)
+                    .background(hovered ? Color(NSColor.separatorColor).opacity(0.8) : .clear)
+                    .cornerRadius(6)
+                    .onHover { hovered in
+                        withAnimation {
+                            self.hovered = hovered
+                        }
+                    }
             }
-        }
-        .buttonStyle(.plain)
-        .padding(2)
-        .background(hovered ? Color(NSColor.separatorColor).opacity(0.8) : .clear)
-        .cornerRadius(6)
-        .onHover { hovered in
-            withAnimation {
-                self.hovered = hovered
+            .buttonStyle(.plain)
+
+            switch router.activeRoute {
+            case .addTimezone:
+                Text("🗺️")
+                    .font(.callout)
+                Text("Add Time Zone")
+                    .font(.body)
+                    .fontWeight(.medium)
+            case .editTimeZone:
+                Text("✍️")
+                    .font(.callout)
+                Text("Edit Time Zone")
+                    .font(.body)
+                    .fontWeight(.medium)
+            case .mainView:
+                EmptyView()
             }
         }
     }
